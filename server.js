5cba0f5718121803abfa29438f87252a8ad12ad3
plansup <plansup.consult@gmail.com>
2024-04-06 14:10:02 +0200

    start form browser map

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 // app.use(express.static(__dirname + '/dist/biodiverstity-footprint'));
-app.use(express.static('./dist/biodiversity-footprint'));
+app.use(express.static('./dist/biodiversity-footprint/browser'));
 
 app.get('/*', (req, res) => {
   // res.sendFile(path.join(__dirname + '/dist/biodiverstity-footprint/index.html'));
-  res.sendFile('index.html', { root: 'dist/biodiverstity-footprint' });
+  res.sendFile('index.html', { root: 'dist/biodiverstity-footprint/browser' });
 });
 
 app.listen(process.env.PORT || 8080);
