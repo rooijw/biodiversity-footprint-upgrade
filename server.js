35f85ca918aea157a18ab861f6e16b325a6217ac
plansup <plansup.consult@gmail.com>
2024-04-06 14:47:01 +0200

    opruimen en weer lokaal kunnen draaien

--- server.js
+++ server.js
@@ -3,11 +3,9 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.static(__dirname + '/dist/biodiverstity-footprint'));
 app.use(express.static('./dist/biodiversity-footprint/browser'));
 
 app.get('/*', (req, res) => {
-  // res.sendFile(path.join(__dirname + '/dist/biodiverstity-footprint/index.html'));
   res.sendFile('index.html', { root: 'dist/biodiverstity-footprint/browser' });
 });
 
